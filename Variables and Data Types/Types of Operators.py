e33b8c665220d16a511b53537968e627a1d44c4e
2023-anik <cse2023anik@gmail.com>
2025-01-23 09:54:35 +0600

    Add Python examples for arithmetic, comparison, and assignment operators; introduce variables and data types

---
+++ Variables and Data Types/Types of Operators.py
@@ -0,0 +1,42 @@
+# Arithmetic Operators
+
+# a = 10
+# b = 3
+# print("Addition:", a,"+", b, "=", a + b)
+# print("Subtraction:", a,"-", b, "=", a - b)
+# print("Multiplication:", a,"*", b, "=", a * b)
+# print("Division:", a,"/", b, "=", a / b) # float division
+# print("Floor Division:", a,"//", b, "=", a // b) # integer division
+# print("Modulus:", a,"%", b, "=", a % b) # remainder
+# print("Exponent:", a,"**", b, "=", a ** b)
+
+
+# Comparison / Relational Operators
+
+# a = 50
+# b = 20
+# print("a:", a, "b:", b)
+# print(a==b) # False
+# print(a!=b) # True
+# print(a>b) # True
+# print(a<b) # False
+# print(a>=b) # True
+# print(a<=b) # False
+
+
+# Assignment Operators
+a = 10
+a += 5 # a = a + 5
+print(a)
+a -= 5 # a = a - 5
+print(a)
+a *= 5 # a = a * 5
+print(a)
+a /= 5 # a = a / 5
+print(a)
+a //= 5 # a = a // 5
+print(a)
+a %= 5 # a = a % 5
+print(a)
+a **= 5 # a = a ** 5
+print(a)
---
+++ Variables and Data Types/variable and data types.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("Hey, I am learning Python.", "It is fun")
+
+# variables and data types
+name = "John" # string 
+age = 25 # integer
+price = 10.5 # float
+is_online = True # boolean
+a = None # None
+
+print(name, age, price, is_online, a)
+
+print("Name:", name, type(name))
+print("Age:", age, type(age))
+print("Price:", price, type(price))
