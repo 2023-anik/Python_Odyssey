00c87f124bc7a313e9ee00352cfd972428ec5692
2023-anik <cse2023anik@gmail.com>
2025-02-09 02:06:30 +0600

    Refactor set storage to differentiate between integer and float values

--- Set Basic/prac2.py
+++ Set Basic/prac2.py
@@ -1,5 +1,11 @@
 #Figure out a way to store 9 & 9.0 as separate values in the set.
 
-a, b = str(9), str(9.0)
-st = (a, b)
+# a, b = str(9), str(9.0)
+# st = (a, b)
+# print(st)
+
+st = {
+    ("float", 9.0),
+    ("int", 9)
+}
 print(st)
